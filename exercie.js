199c600a57f9fe5b4ab90b54c044e09c8887761d
essadbenslama <benslamaessad93@gmail.com>
2023-10-10 19:17:37 +0100

    firstjava

---
+++ exercie.js
@@ -0,0 +1,20 @@
+let num1=2;
+let num2=5;
+let test1=num1%2;
+let test2=num2%2;
+let result1=test1=true;
+let result2=test2=false;
+console.log(`is ${num1} an even number?${result1}`);
+console.log(`is ${num2} an even number?${result2}`);
+
+
+let now="Now in";
+let three="3";
+let d="D";
+console.log(`${now} ${three}${d}`);
+
+
+let counter=0;
+counter+=5;
+counter+=3
+console.log (counter);
