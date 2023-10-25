9a0d44385b4d5aa6b7dc8da2b81fe16fd570d5ec
essadbenslama <benslamaessad93@gmail.com>
2023-10-25 15:40:38 +0100

    reverse

--- exercie.js
+++ exercie.js
@@ -1,20 +1,18 @@
-let num1=2;
-let num2=5;
-let test1=num1%2;
-let test2=num2%2;
-let result1=test1=true;
-let result2=test2=false;
-console.log(`is ${num1} an even number?${result1}`);
-console.log(`is ${num2} an even number?${result2}`);
 
+// function inverseword(str) {
+// let reversedWord=str.toLowerCase().split(' ').reverse().join('')
+// if(str.toLowerCase()===reversedWord){
+//     return true
+// }else {return false }
 
-let now="Now in";
-let three="3";
-let d="D";
-console.log(`${now} ${three}${d}`);
+// }
+// console.log(inverseword('kayak'))
+	
 
+function inverse(str){
+    let istr=str.toLowerCase().split(' ').reverse().join('')
+    if(str.toLowerCase()===istr){return true}
+    else{return false}
 
-let counter=0;
-counter+=5;
-counter+=3
-console.log (counter);
+}
+console.log(inverse('kayak'))
